c5646d098c5ddd7677f84c6217d6e3dd5671e1c5
StiLKnoT <stilknot@gmail.com>
2022-10-02 12:19:42 +0500

    add link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ import {Link} from 'react-router-dom'
 function Navbar() {
     return (
         <nav className='flex justify-between items-center h-[50px] px-5 shadow-md bg-gray-500 text-white'>
-            <h3 className='font-bold'>Github Search</h3>
+            <h3 className='font-bold'><Link to='/'>Github Search</Link></h3>
 
             <span>
                 <Link to='/' className='mr-2'>Home</Link>
